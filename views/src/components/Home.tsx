1f8df7bc17ae18d4cdb3d0881665d401e366edd2
breno12filipe <brenofilipe1212@gmail.com>
2024-02-02 01:13:04 -0300

    Adding Home components

---
+++ views/src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const HomePage = () => {
+  return (
+    <h1>Home Page</h1>
+  );
+};
+
+export default HomePage;

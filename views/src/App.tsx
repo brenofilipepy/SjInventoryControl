63b7118708d8a92ea343fbab8e9d6997ea594ce8
breno12filipe <brenofilipe1212@gmail.com>
2024-02-02 02:48:30 -0300

    Adding concurrently to run both express and react server at the same time

--- views/src/App.tsx
+++ views/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import LoginPage from './components/Login.js'
+import LoginPage from './components/Login.tsx'
 
 export default function App(): ReactNode {
   return (
